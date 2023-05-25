cb34310c4b78cbdd4e022ae1c9e89f4fd98da89b
dhaDang <130107069+dhaDang@users.noreply.github.com>
2023-05-25 15:18:30 -0700

    changed

--- grade.sh
+++ grade.sh
@@ -5,12 +5,38 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission 
+#$1 takes first argument and clones 
 echo 'Finished cloning'
-
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+
+if [[ -f student-submission/ListExamples.java ]]
+    then 
+    echo "ListExamples.java found"
+    else  
+    echo "ListExamples.java not found"
+    echo "Score:0/4"
+    exit
+fi
+
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area #copying recursively and everything in library folder to grading area 
+
+cd grading-area
+
+javac -cp $CPATH grading-area/*.java 
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+
+
+
+
+
+
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
